83b13f20406097d3317201f6f81048fec5f7387f
Jessica <jessica_costaignacio@hotmail.com>
2024-05-03 13:05:01 -0300

    CRUD de Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './categoria/entities/categoria.entity';
-import { Produto } from './produto/entities/produto.entity';
 import { CategoriaModule } from './categoria/categoria.module';
-import { ProdutoModule } from './produto/produto.module';
-
 
 @Module({
   imports: [
@@ -15,11 +12,10 @@ import { ProdutoModule } from './produto/produto.module';
       username: 'root',
       password: 'Lana5595',
       database: 'db_farmacia',
-      entities: [Categoria, Produto],
+      entities: [Categoria],
       synchronize: true  
     }),
     CategoriaModule,
-    ProdutoModule
   ],
   controllers: [],
   providers: [],
--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,6 +1,5 @@
 import { IsNotEmpty } from "class-validator"
-import { Produto } from "src/produto/entities/produto.entity"
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity({name: "tb_categorias"})
 export class Categoria {
@@ -11,9 +10,6 @@ export class Categoria {
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
     categoria: string
-
-    @ManyToOne(() => Produto, (produto) => produto.categoria)
-    produto: Produto[]
  
 }
 
